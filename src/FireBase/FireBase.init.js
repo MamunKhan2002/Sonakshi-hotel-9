8cd72ff03839c2c2a5e856e4ea973c0938b3e64a
MamunKhan2002 <mamunkhan2002bd@gmail.com>
2024-05-25 18:47:29 +0600

    some edit

--- src/FireBase/FireBase.init.js
+++ src/FireBase/FireBase.init.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 // console.log(import.meta.env.VITE_PASS);
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmLyqfo7YPAvAbPJPbE51bW6PNVRQDTcE",
-  authDomain: "sonaksi-hotel.firebaseapp.com",
-  projectId: "sonaksi-hotel",
-  storageBucket: "sonaksi-hotel.appspot.com",
-  messagingSenderId: "47214537941",
-  appId: "1:47214537941:web:ecf015f2d8b8331f7c865c"
+
 };
 
 const app = initializeApp(firebaseConfig);
